44a299203f337dfc863569d6203b73d528d478d0
umut9191 <umutsurmeli91@gmail.com>
2022-02-20 22:34:20 +0300

    Initial Commit

---
+++ SwiftUIAssets/SwiftUIAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAssetsApp.swift
+//  SwiftUIAssets
+//
+//  Created by Macintosh on 20.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
