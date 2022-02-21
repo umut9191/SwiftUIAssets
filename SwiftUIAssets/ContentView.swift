dd9126f354c37b9d8337249412114fe3ad879034
umut9191 <umutsurmeli91@gmail.com>
2022-02-22 00:23:18 +0300

    image extension, image placeholder, image phase ; (success, error, empty ) codes added

--- SwiftUIAssets/ContentView.swift
+++ SwiftUIAssets/ContentView.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+extension Image{
+    func ImageModifier() -> some View{
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func IconModifier() -> some View{
+        self
+            .ImageModifier()
+            .frame(maxWidth:128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
+
 struct ContentView: View {
     private let imageUrl:String = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
@@ -14,8 +31,46 @@ struct ContentView: View {
        // AsyncImage(url: URL(string: imageUrl))
         
         //MARK: - 2. Scale
-        AsyncImage(url: URL(string: imageUrl),scale: 3.0)
+        //AsyncImage(url: URL(string: imageUrl),scale: 3.0)
         //default scale value 1.0, greater value will small the image
+        
+        //MARK: - 3. Placeholder
+//        AsyncImage(url: URL(string: imageUrl)) {image in
+//            image
+//                .resizable()
+//                .scaledToFit()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(maxWidth:128)
+//                .foregroundColor(.purple)
+//                .opacity(0.5)
+//        }.padding(40)
+        
+    //MARK:- 4. Using extension
+//        AsyncImage(url: URL(string: imageUrl)) {image in
+//            image.ImageModifier()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill")
+//                .IconModifier()
+//        }.padding(40)
+        
+        //MARK:- 4. Phase
+            AsyncImage(url: URL(string: imageUrl)) {phase in
+                //SUCCESS: The image succesfully loaded.
+                //FAILURE: The image failed to load with an error
+                //EMPTY: No images loaded
+                if let image = phase.image {
+                    image.ImageModifier()
+                }
+                else if phase.error != nil{
+                    Image(systemName: "ant.circle.fill").IconModifier()
+                }
+                else {
+                    Image(systemName: "photo.circle.fill").IconModifier()
+                }
+            }.padding(40)
     }
 }
 
